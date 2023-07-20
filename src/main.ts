12f91a26c94b7e77c13c6690053f06650a0c6b96
Evgeni <126806194+EvGenI191@users.noreply.github.com>
2023-07-21 01:30:13 +0300

    vite

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,10 @@ import './style.css'
 
 let name = prompt(`введите свое имя`)
 alert(`«Привет,${name}`)
+
+let years = prompt(`в каком году вы родились?`)
+let year 
+year = 2023
+let yeaar = year - years
+alert(`Вам,${yeaar}`)
+
