31faa88eb041668f3d07ce482ca1a89284e664f6
Evgeni <126806194+EvGenI191@users.noreply.github.com>
2023-07-27 00:16:51 +0300

    vite

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,24 @@
 import './style.scss'
 import './style.css'
 
-let name = prompt(`введите свое имя`)
-alert(`«Привет,${name}`)
+let name = prompt(`введите свое имя`);
+alert(`«Привет,${name}`);
 
-let years = prompt(`в каком году вы родились?`)
+let years = prompt(`в каком году вы родились?`);
 let year 
 year = 2023
 let yeaar = year - years
-alert(`Вам,${yeaar}`)
+alert(`Вам,${yeaar}`);
 
-let long = prompt(`Введите длину стороны квадрата`)
+let long = prompt(`Введите длину стороны квадрата`);
 let perim = long * 4
-alert(`Периметр квадрата равен,${perim}`)
+alert(`Периметр квадрата равен,${perim}`);
 
+let radius = numb(prompt(`Введите радиус окружности: `));
+let square = Math.PI * radius ** 2;
+alert(`Площадь окружности:`+ square);
 
-
-let mile = prompt(`Введите расстояние между объектами`)
-let time = prompt(`Введите время за которое хотите добраться до место назначения`)
+let mile = prompt(`Введите расстояние между объектами`);
+let time = prompt(`Введите время за которое хотите добраться до место назначения`);
 let speed = mile / time
-alert(`Вам нужно двигатся с этой скоростью,${speed},km/h`)
+alert(`Вам нужно двигатся с этой скоростью,${speed},km/h`);
